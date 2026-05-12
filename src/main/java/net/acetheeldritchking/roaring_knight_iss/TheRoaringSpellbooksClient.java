a55d646d5560bca905d7a02af0c653e1bbb4c816
Ace The King <solcat0414@gmail.com>
2026-05-12 17:37:25 -0400

    Changelog:
    Dependancies setup

--- src/main/java/net/acetheeldritchking/roaring_knight_iss/TheRoaringSpellbooksClient.java
+++ src/main/java/net/acetheeldritchking/roaring_knight_iss/TheRoaringSpellbooksClient.java
@@ -25,7 +25,5 @@ public class TheRoaringSpellbooksClient {
     @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
         // Some client setup code
-        TheRoaringSpellbooks.LOGGER.info("HELLO FROM CLIENT SETUP");
-        TheRoaringSpellbooks.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
 }
